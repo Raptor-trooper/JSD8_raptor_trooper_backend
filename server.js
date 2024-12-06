3d7d2e4f970e0f986d8d06520b11534cdecf77f3
Taejin <taerattanakun@gmail.com>
2024-12-06 10:40:00 +0700

    Create Structure Backend

--- server.js
+++ server.js
@@ -1,1 +1,51 @@
-eiei;
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+connectDB();
+connectCloudinary();
+
+app.use(express.json());
+const allowedOrigins = [
+  "https://admin-seven-coral.vercel.app",
+  "https://user-six-bay.vercel.app",
+  "http://localhost:5175",
+  "http://localhost:5176",
+  "http://localhost:5173",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+app.get("/", (req, res) => res.send("API Working"));
+
+app.listen(port, () =>
+  console.log("Server started on PORT : http://localhost:" + port + "🌎👋")
+);
