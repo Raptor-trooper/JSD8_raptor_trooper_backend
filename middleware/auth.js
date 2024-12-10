ca087fa1972ff1a72538c74b7a7c9e40e05f3e74
Rawisut06 <tungtung0685@gmail.com>
2024-12-10 21:17:31 +0700

    edited middleware auth and userController

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,20 @@
 import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
 
 const authUser = async (req, res, next) => {
-  const { token } = req.headers;
-  if (!token) {
-    return res.json({ success: false, message: "Not Authorized Login Again" });
-  }
   try {
-    const token_decode = jwt.verify(token, process.env.JWT_SECRET);
-    req.body.userId = token_decode.id;
+    const authHeader = req.headers['Authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+      return res.json({ success: false, message: "Unauthorized: Login Again" });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await userModel.findById(decoded.user._id);
+    if (!user) {
+      return res.json({ success: false, message : "Unauthorized: User not found" });
+    }
+    req.user = user;
     next();
   } catch (error) {
     console.log(error);
