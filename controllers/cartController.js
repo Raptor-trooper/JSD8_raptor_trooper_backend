d3cdf5ba951cbfa8535f8efe8af017fa8a0ab698
Taejin <taerattanakun@gmail.com>
2024-12-12 00:41:09 +0700

    Update Feature updateCart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -28,16 +28,33 @@ const addToCart = async (req, res) => {
 };
 
 // update user cart
+// const updateCart = async (req, res) => {
+//   try {
+//     const { userId, itemId, quantity } = req.body;
+
+//     const userData = await userModel.findById(userId);
+//     let cartData = await userData.cartData;
+
+//     cartData[itemId] = quantity;
+
+//     await userModel.findByIdAndUpdate(userId, { cartData });
+//     res.json({ success: true, message: "Cart Updated" });
+//   } catch (error) {
+//     console.log(error);
+//     res.json({ success: false, message: error.message });
+//   }
+// };
+
 const updateCart = async (req, res) => {
   try {
     const { userId, itemId, quantity } = req.body;
 
     const userData = await userModel.findById(userId);
-    let cartData = await userData.cartData;
+    // let cartData = await userData.cartData;
 
-    cartData[itemId] = quantity;
+    // cartData[itemId] = quantity;
 
-    await userModel.findByIdAndUpdate(userId, { cartData });
+    await userModel.findByIdAndUpdate(userId, { cartData: quantity });
     res.json({ success: true, message: "Cart Updated" });
   } catch (error) {
     console.log(error);
