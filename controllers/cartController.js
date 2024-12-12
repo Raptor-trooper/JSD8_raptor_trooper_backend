4a43753036778bcec3731e3cf2971a034d99413f
Taejin <taerattanakun@gmail.com>
2024-12-12 14:04:40 +0700

    Reuse cartController function updateCart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -28,33 +28,16 @@ const addToCart = async (req, res) => {
 };
 
 // update user cart
-// const updateCart = async (req, res) => {
-//   try {
-//     const { userId, itemId, quantity } = req.body;
-
-//     const userData = await userModel.findById(userId);
-//     let cartData = await userData.cartData;
-
-//     cartData[itemId] = quantity;
-
-//     await userModel.findByIdAndUpdate(userId, { cartData });
-//     res.json({ success: true, message: "Cart Updated" });
-//   } catch (error) {
-//     console.log(error);
-//     res.json({ success: false, message: error.message });
-//   }
-// };
-
 const updateCart = async (req, res) => {
   try {
     const { userId, itemId, quantity } = req.body;
 
     const userData = await userModel.findById(userId);
-    // let cartData = await userData.cartData;
+    let cartData = await userData.cartData;
 
-    // cartData[itemId] = quantity;
+    cartData[itemId] = quantity;
 
-    await userModel.findByIdAndUpdate(userId, { cartData: quantity });
+    await userModel.findByIdAndUpdate(userId, { cartData });
     res.json({ success: true, message: "Cart Updated" });
   } catch (error) {
     console.log(error);
@@ -62,7 +45,25 @@ const updateCart = async (req, res) => {
   }
 };
 
+// const updateCart = async (req, res) => {
+//   try {
+//     const { userId, itemId, quantity } = req.body;
+
+//     const userData = await userModel.findById(userId);
+//     // let cartData = await userData.cartData;
+
+//     // cartData[itemId] = quantity;
+
+//     await userModel.findByIdAndUpdate(userId, { cartData: quantity });
+//     res.json({ success: true, message: "Cart Updated" });
+//   } catch (error) {
+//     console.log(error);
+//     res.json({ success: false, message: error.message });
+//   }
+// };
+
 // get user cart data
+
 const getUserCart = async (req, res) => {
   try {
     const { userId } = req.body;
