c373e58172473a7d8804d44be919dbed873d633d
rawisut06 <tungtung0685@gmail.com>
2024-12-17 23:34:49 +0700

    added checkrole

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,6 +4,7 @@ import {
   addProduct,
   removeProduct,
   singleProduct,
+  checkRole
 } from "../controllers/productController.js";
 import upload from "../middleware/multer.js";
 import adminAuth from "../middleware/adminAuth.js";
@@ -12,17 +13,18 @@ const productRouter = express.Router();
 
 productRouter.post(
   "/add",
-  // adminAuth,
+  adminAuth,
+  addProduct,
   upload.fields([
     { name: "image1", maxCount: 1 },
     { name: "image2", maxCount: 1 },
     { name: "image3", maxCount: 1 },
     { name: "image4", maxCount: 1 },
   ]),
-  addProduct
 );
 productRouter.post("/remove", adminAuth, removeProduct);
 productRouter.post("/single", adminAuth, singleProduct);
+productRouter.post("/checkrole", adminAuth, checkRole);
 
 productRouter.get("/list", listProducts);
 
